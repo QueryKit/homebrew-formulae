5e35ae52536e751c1dd374c8b2f724383081b974
Kyle Fuller <kyle@fuller.li>
2015-09-15 13:28:15 -0700

    [querykit-cli] Release 0.1.3

--- Formula/querykit-cli.rb
+++ Formula/querykit-cli.rb
@@ -1,8 +1,8 @@
 class QuerykitCli < Formula
   homepage "http://querykit.org/"
   head "https://github.com/kylef/querykit-cli"
-  url "https://github.com/kylef/querykit-cli/releases/download/0.1.2/querykit-cli.tar.gz"
-  sha256 "a4b6198cca190bc3dc5adb9d9de9326c4e29fbf609036118a24de0e5a3c3255e"
+  url "https://github.com/kylef/querykit-cli/releases/download/0.1.3/querykit-cli.tar.gz"
+  sha256 "8f0b2d55eba60f10f4c075009b81cd082cbd254e0d6726614bae1be5359d07d8"
 
   def install
     system "make" if build.head?
